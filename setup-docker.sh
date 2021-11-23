c0f54a4c18f81f0128d43f89ff2da9eca636e299
Ratko Dimovski <d_ratko30@yahoo.com>
2021-11-23 20:02:12 +0100

    cont name update

--- setup-docker.sh
+++ setup-docker.sh
@@ -10,4 +10,4 @@ docker network create food-net
 docker run -d --name es --net food-net -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:7.15.2
 
 # start the flask app container
-docker run -d --net food-net -p 5500:5000 --name foodtrucks-web dratko30/food-web
+docker run -d --net food-net -p 5500:5000 --name food-web dratko30/food-web
